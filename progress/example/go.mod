61b0b9b118312c0c70345a3e6a1cb8bb2b59e48e
Christian Rocha <christian@rocha.is>
2020-11-18 18:05:22 -0500

    Add progress bar example

---
+++ progress/example/go.mod
@@ -0,0 +1,10 @@
+module github.com/charmbracelet/progress/example
+
+replace github.com/charmbracelet/bubbles => ../../
+
+go 1.15
+
+require (
+	github.com/charmbracelet/bubbles v0.0.0-00010101000000-000000000000 // indirect
+	github.com/muesli/reflow v0.2.0 // indirect
+)
---
+++ progress/example/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	stepSize float64 = 1.0 / (60.0 * 2.0)
+	padding          = 2
+	maxWidth         = 80
+)
+
+func main() {
+	progress := progress.NewModel(
+		progress.WithDefaultRamp(),
+	)
+
+	if err := tea.NewProgram(example{progress: progress}).Start(); err != nil {
+		fmt.Println("Oh no!", err)
+		os.Exit(1)
+	}
+}
+
+type tickMsg time.Time
+
+type example struct {
+	percent  float64
+	progress *progress.Model
+}
+
+func (e example) Init() tea.Cmd {
+	return tickCmd()
+}
+
+func (e example) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return e, tea.Quit
+		default:
+			return e, nil
+		}
+
+	case tea.WindowSizeMsg:
+		e.progress.Width = msg.Width - padding*2 - 4
+		if e.progress.Width > maxWidth {
+			e.progress.Width = maxWidth
+		}
+		return e, nil
+
+	case tickMsg:
+		e.percent += stepSize
+		if e.percent > 1.0 {
+			e.percent = 1.0
+			return e, tea.Quit
+		}
+		return e, tickCmd()
+
+	default:
+		return e, nil
+	}
+}
+
+func (e example) View() string {
+	pad := strings.Repeat(" ", padding)
+	return "\n" + pad + e.progress.View(e.percent) + "\n\n"
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second/60, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
