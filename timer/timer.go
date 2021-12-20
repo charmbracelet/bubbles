203611ad5b14b9c0f85fc99a96ef2d6d5dc6af66
Christian Rocha <christian@rocha.is>
2021-12-20 16:44:57 -0500

    Various timer improvements
    * Add IDs to timers
    * Ignore messages from other timers
    * Add timeout property to TickMsg
    * Also send a TickMsg with timeout flag on timeout
    * Add start/stop commands
    * Add Timedout() method to timer model
    * Add Running() method to timer model

--- timer/timer.go
+++ timer/timer.go
@@ -2,16 +2,47 @@
 package timer
 
 import (
+	"sync"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+var (
+	lastID int
+	idMtx  sync.Mutex
+)
+
+func nextID() int {
+	idMtx.Lock()
+	defer idMtx.Unlock()
+	lastID++
+	return lastID
+}
+
 // TickMsg is a message that is sent on every timer tick.
-type TickMsg struct{}
+type TickMsg struct {
+	// ID is the identifier of the stopwatch that send the message. This makes
+	// it possible to determine which timer a tick belongs to when there
+	// are multiple timers running.
+	//
+	// Note, however, that a timer will reject ticks from other stopwatches, so
+	// it's safe to flow all TickMsgs through all timers and hvae them still
+	// behave appropriately.
+	ID int
+
+	// Timeout returns whether or not this tick is a timeout tick. You can
+	// alternatively listen for TimeoutMsg.
+	Timeout bool
+}
 
 // TimeoutMsg is a message that is sent once when the timer times out.
-type TimeoutMsg struct{}
+//
+// It's a convenience message sent alongside a TickMsg with the Timeout value
+// set to true.
+type TimeoutMsg struct {
+	ID int
+}
 
 // Model of the timer component.
 type Model struct {
@@ -20,6 +51,9 @@ type Model struct {
 
 	// How long to wait before every tick. Defaults to 1 second.
 	Interval time.Duration
+
+	id      int
+	running bool
 }
 
 // NewWithInterval creates a new timer with the given timeout and tick interval.
@@ -27,6 +61,7 @@ func NewWithInterval(timeout, interval time.Duration) Model {
 	return Model{
 		Timeout:  timeout,
 		Interval: interval,
+		running:  true,
 	}
 }
 
@@ -35,22 +70,45 @@ func New(timeout time.Duration) Model {
 	return NewWithInterval(timeout, time.Second)
 }
 
+// ID returns
+func (m Model) ID() int {
+	return m.id
+}
+
+// Running returns whether or not the timer is running. If the timer has timed
+// out this will always return false.
+func (m Model) Running() bool {
+	if m.Timedout() || !m.running {
+		return false
+	}
+	return true
+}
+
+// Timedout returns whether or not the timer has timed out.
+func (m Model) Timedout() bool {
+	return m.Timeout <= 0
+}
+
 // Init starts the timer.
 func (m Model) Init() tea.Cmd {
-	return tick(m.Interval)
+	return tick(m.id, m.Interval, m.Timedout())
 }
 
 // Update handles the timer tick.
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
-	switch msg.(type) {
+	switch msg := msg.(type) {
 	case TickMsg:
-		m.Timeout -= m.Interval
-		if m.Timeout <= 0 {
-			return m, func() tea.Msg {
-				return TimeoutMsg{}
-			}
+		if !m.Running() || (msg.ID != 0 && msg.ID != m.id) {
+			break
 		}
-		return m, tick(m.Interval)
+
+		m.Timeout -= m.Interval
+		tickCmd := tick(m.id, m.Interval, m.Timedout())
+
+		if m.Timedout() {
+			return m, tea.Batch(tickCmd, timeout(m.id))
+		}
+		return m, tickCmd
 	}
 
 	return m, nil
@@ -61,8 +119,39 @@ func (m Model) View() string {
 	return m.Timeout.String()
 }
 
-func tick(d time.Duration) tea.Cmd {
+// Start resumes the timer. Has no effect if the timer has timed out.
+func (m *Model) Start() tea.Cmd {
+	m.running = true
+	if m.Timedout() {
+		return nil
+	}
+	return tick(m.id, m.Interval, m.Timedout())
+}
+
+// Stop pauses the timer. Has no effect if the timer has timed out.
+func (m *Model) Stop() tea.Cmd {
+	m.running = false
+	return func() tea.Msg {
+		return nil
+	}
+}
+
+// Toggle stops the timer if it's running and starts it if it's stopped.
+func (m *Model) Toggle() tea.Cmd {
+	if m.Running() {
+		return m.Stop()
+	}
+	return m.Start()
+}
+
+func tick(id int, d time.Duration, timedout bool) tea.Cmd {
 	return tea.Tick(d, func(_ time.Time) tea.Msg {
-		return TickMsg{}
+		return TickMsg{ID: id, Timeout: timedout}
 	})
+}
+
+func timeout(id int) tea.Cmd {
+	return func() tea.Msg {
+		return TimeoutMsg{ID: id}
+	}
 }
