93aebd655953e4a00dcc05117cda15590e59728f
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2022-07-08 17:01:03 -0300

    test: FromValues method in table

---
+++ table/table_test.go
@@ -0,0 +1,36 @@
+package table
+
+import "testing"
+
+func TestFromValues(t *testing.T) {
+	input := "foo1,bar1\nfoo2,bar2\nfoo3,bar3"
+	table := New(WithColumns([]Column{{Title: "Foo"}, {Title: "Bar"}}))
+	table.FromValues(input, ",")
+
+	if len(table.rows) != 3 {
+		t.Fatalf("expect table to have 3 rows but it has %d", len(table.rows))
+	}
+
+	expect := []Row{
+		{"foo1", "bar1"},
+		{"foo2", "bar2"},
+		{"foo3", "bar3"},
+	}
+	if !deepEqual(table.rows, expect) {
+		t.Fatal("table rows is not equals to the input")
+	}
+}
+
+func deepEqual(a, b []Row) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, r := range a {
+		for j, f := range r {
+			if f != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
