3b8e2d30a931b1b09859368158f0ba1b3f158a1b
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2022-07-08 17:40:53 -0300

    add another test caso

--- table/table_test.go
+++ table/table_test.go
@@ -21,6 +21,24 @@ func TestFromValues(t *testing.T) {
 	}
 }
 
+func TestFromValuesWithTabSeparator(t *testing.T) {
+	input := "foo1.\tbar1\nfoo,bar,baz\tbar,2"
+	table := New(WithColumns([]Column{{Title: "Foo"}, {Title: "Bar"}}))
+	table.FromValues(input, "\t")
+
+	if len(table.rows) != 2 {
+		t.Fatalf("expect table to have 2 rows but it has %d", len(table.rows))
+	}
+
+	expect := []Row{
+		{"foo1.", "bar1"},
+		{"foo,bar,baz", "bar,2"},
+	}
+	if !deepEqual(table.rows, expect) {
+		t.Fatal("table rows is not equals to the input")
+	}
+}
+
 func deepEqual(a, b []Row) bool {
 	if len(a) != len(b) {
 		return false
