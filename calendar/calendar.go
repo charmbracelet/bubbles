01d9e22bc66f2cf687d00ed934c8c26f51c2f69d
gfeun <gfeun@protonmail.com>
2022-08-18 23:45:15 +0200

    Support multiple months

--- calendar/calendar.go
+++ calendar/calendar.go
@@ -1,12 +1,13 @@
 /*
 Calendar component
 
-Mo Tu We Th Fr Sa Su
-             1  2  3
- 4  5  6  7  8  9 10
-11 12 13 14 15 16 17
-18 19 20 21 22 23 24
-25 26 27 28 29 30 31
+July                  August                September
+Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su
+             1  2  3   1  2  3  4  5  6  7            1  2  3  4
+ 4  5  6  7  8  9 10   8  9 10 11 12 13 14   5  6  7  8  9 10 11
+11 12 13 14 15 16 17  15 16 17 18 19 20 21  12 13 14 15 16 17 18
+18 19 20 21 22 23 24  22 23 24 25 26 27 28  19 20 21 22 23 24 25
+25 26 27 28 29 30 31  29 30 31              26 27 28 29 30
 */
 
 package calendar
@@ -20,11 +21,14 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+const columnWidth = 3
+
 // Model stores component.
 type Model struct {
-	CurrentDate time.Time
-	Styles      Styles
-	Weekdays    []Weekday
+	CurrentDate      time.Time
+	Styles           Styles
+	Weekdays         []Weekday
+	NbMonthDisplayed int
 }
 
 // Styles stores all component styles.
@@ -56,11 +60,12 @@ func NewModel() Model {
 	return Model{
 		CurrentDate: time.Now(),
 		Styles: Styles{
-			CurrentDate:    lipgloss.NewStyle().ColorWhitespace(false).Width(3).Align(lipgloss.Center).Background(lipgloss.Color("#7571F9")),
-			Date:           lipgloss.NewStyle().ColorWhitespace(false).Width(3).Align(lipgloss.Center),
-			WeekdaysHeader: lipgloss.NewStyle().ColorWhitespace(false).Width(3).Align(lipgloss.Left).Background(lipgloss.Color("#F25D94")),
+			CurrentDate:    lipgloss.NewStyle().ColorWhitespace(false).Width(columnWidth).Align(lipgloss.Center).Background(lipgloss.Color("#7571F9")),
+			Date:           lipgloss.NewStyle().ColorWhitespace(false).Width(columnWidth).Align(lipgloss.Center),
+			WeekdaysHeader: lipgloss.NewStyle().ColorWhitespace(false).Width(columnWidth).Align(lipgloss.Left).Background(lipgloss.Color("#F25D94")),
 		},
-		Weekdays: EnglishWeekdays,
+		Weekdays:         EnglishWeekdays,
+		NbMonthDisplayed: 3,
 	}
 }
 
@@ -82,43 +87,90 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View method.
 func (m Model) View() string {
-	firstofmonth := time.Date(m.CurrentDate.Year(), m.CurrentDate.Month(), 1, 0, 0, 0, 0, m.CurrentDate.Location())
-	lastofmonth := firstofmonth.AddDate(0, 1, -1)
+	// Each month will have their days represented in a string array
+	calendarMonthRender := make([][]string, m.NbMonthDisplayed)
+
+	for monthIndex := range calendarMonthRender {
+		firstDayOfMonth := time.Date(m.CurrentDate.Year(), m.CurrentDate.Month(), 1, 0, 0, 0, 0, m.CurrentDate.Location())
+
+		// Use an index relative to the current month. 0 is current month, -1 is the month before, +1 is the month after etc.
+		// This makes it easy to calculate first and last days for each months in our calendar
+		monthRelativePosition := monthIndex - (len(calendarMonthRender) / 2)
+		firstDayOfMonth = firstDayOfMonth.AddDate(0, monthRelativePosition, 0)
+		lastDayOfMonth := firstDayOfMonth.AddDate(0, 1, -1)
+
+		// Month name heading and days
+		// July                  August                September
+		// Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su  Mo Tu We Th Fr Sa Su
+		s := ""
+		s += firstDayOfMonth.Month().String()
+		s += "\n"
+
+		for i := 0; i < len(m.Weekdays); i++ {
+			s += m.Styles.WeekdaysHeader.Render(m.Weekdays[i].Abbreviation)
+		}
+		s += "\n"
+
+		// Determine 1st day in the month position in the week to complete with padding
+		// Dashes represent the required offset padding (offset * weekday header width):
+		// Mo Tu We Th Fr Sa Su
+		// ------------ 1  2  3
+		//  4  5  6  7  8  9 10
+		monthStartingDayOffset := 0
+		for i, weekday := range m.Weekdays {
+			if weekday.Name == firstDayOfMonth.Weekday().String() {
+				monthStartingDayOffset = i
+				break
+			}
+		}
+		s += strings.Repeat("   ", monthStartingDayOffset)
+
+		for i := 1; i <= lastDayOfMonth.Day(); i++ {
+			// Current selected day is highlighted
+			if i == m.CurrentDate.Day() && monthRelativePosition == 0 {
+				s += m.Styles.CurrentDate.Render(strconv.Itoa(i))
+			} else {
+				s += m.Styles.Date.Render(strconv.Itoa(i))
+			}
+
+			// Add a line return on week end to prepare new line
+			// Except when the last day in the month ends on the last weekday
+			if (i+monthStartingDayOffset)%7 == 0 && i != lastDayOfMonth.Day() {
+				s += "\n"
+			}
+		}
+
+		s += "\n"
+
+		calendarMonthRender[monthIndex] = strings.Split(s, "\n")
+	}
 
 	s := ""
-	for i := 0; i < len(m.Weekdays); i++ {
-		s += m.Styles.WeekdaysHeader.Render(m.Weekdays[i].Abbreviation)
-	}
-	s += "\n"
-
-	// Determine 1st day in the month position in the week to complete with padding
-	// Dashes represent the required offset padding (offset * weekday header width):
-	// Mo Tu We Th Fr Sa Su
-	// ------------ 1  2  3
-	//  4  5  6  7  8  9 10
-	monthStartingDayOffset := 0
-	for i, weekday := range m.Weekdays {
-		if weekday.Name == firstofmonth.Weekday().String() {
-			monthStartingDayOffset = i
-			break
+	maxNbLine := 0
+	for _, month := range calendarMonthRender {
+		if len(month) > maxNbLine {
+			maxNbLine = len(month)
 		}
 	}
-	s += strings.Repeat("   ", monthStartingDayOffset)
 
-	// Render calendar
-	// Current selected day is highlighted
-	for i := 1; i <= lastofmonth.Day(); i++ {
-		if i == m.CurrentDate.Day() {
-			s += m.Styles.CurrentDate.Render(strconv.Itoa(i))
-		} else {
-			s += m.Styles.Date.Render(strconv.Itoa(i))
-		}
+	// Render calendar by iterating over each month, line by line
+	// For example if we have three months, our first line will look like this:
+	//        1  2  3  4  5               1  2  3   1  2  3  4  5  6  7
+	// The second line will look like this:
+	//  6  7  8  9 10 11 12   4  5  6  7  8  9 10   8  9 10 11 12 13 14
 
-		// Line return on week end, except when the last day in the month ends on the last weekday
-		if (i+monthStartingDayOffset)%7 == 0 && i != lastofmonth.Day() {
-			s += "\n"
+	for i := 0; i < maxNbLine; i++ {
+		for monthPosition, month := range calendarMonthRender {
+			// Padding between months
+			if monthPosition > 0 {
+				s += " "
+			}
+			// Render calendar line
+			if i < len(month) {
+				s += lipgloss.NewStyle().Width(columnWidth * 7).Render(month[i])
+			}
 		}
+		s += "\n"
 	}
-	s += "\n"
 	return s
 }
