37a5b4cf28dc0e8aa2fab0f5bdfc15312dac6224
KaviiSuri <surikavii@gmail.com>
2023-02-19 21:08:51 +0530

    moved initialized to screen

--- router/router.go
+++ router/router.go
@@ -9,13 +9,13 @@ import (
 type Screen struct {
 	model   tea.Model
 	binding key.Binding
+  initialized bool
 }
 
 // Model stores different screens and the currently focused screens
 type Model struct {
 	screens     []Screen
 	current     int
-	initialized []bool
 }
 
 // New creates a new empty Model
@@ -26,11 +26,9 @@ func New() Model {
 // NewWithScreens creates a new model with an array of Screen
 func NewWithScreens(screens []Screen) Model {
 	current := 0
-	initialized := make([]bool, len(screens))
 	return Model{
 		screens,
 		current,
-		initialized,
 	}
 }
 
@@ -45,8 +43,8 @@ func (m Model) updateCurrent(msg tea.Msg) (Model, tea.Cmd) {
 
 // AddScreen adds a new screen to the router
 func (m *Model) AddScreen(model tea.Model, binding key.Binding) {
-	m.screens = append(m.screens, Screen{model, binding})
-	m.initialized = append(m.initialized, false)
+  initialized := false
+	m.screens = append(m.screens, Screen{model, binding, initialized})
 }
 
 // setCurrent sets the current screen to the given integer and initializes the screen if not already initailized
@@ -55,8 +53,8 @@ func (m *Model) setCurrent(current int) tea.Cmd {
 		return nil
 	}
 	m.current = current
-	if !m.initialized[m.current] {
-		m.initialized[m.current] = true
+	if !m.screens[m.current].initialized {
+		m.screens[m.current].initialized = true
 		return m.screens[m.current].model.Init()
 	}
 	return nil
