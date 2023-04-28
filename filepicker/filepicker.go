ac12991751ba5d03c05e027ee5ca5ae6d4252075
Kirn Kim <kirnkim@gmail.com>
2023-04-28 11:17:37 -0700

    fix render bug

--- filepicker/filepicker.go
+++ filepicker/filepicker.go
@@ -24,13 +24,13 @@ var (
 	singlePaneStyle          = lipgloss.NewStyle().
 					Border(lipgloss.NormalBorder(), false, false, false, false).
 					MarginRight(2).
-					Height(listHeight - 4).
+					Height(listHeight - 2).
 					Width(listWidth - 2)
 
 	dualPaneStyle = lipgloss.NewStyle().
 			Border(lipgloss.NormalBorder(), false, true, false, false).
 			MarginRight(2).
-			Height(listHeight - 4).
+			Height(listHeight - 2).
 			Width(listWidth/2 - 2)
 )
 
@@ -257,19 +257,9 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		if m.AutoHeight {
 			m.Height = msg.Height - marginBottom
 		}
-		m.max = m.Height
-		singlePaneStyle = singlePaneStyle.Height(m.Height - 4).Width(listWidth - 2)
-		dualPaneStyle = dualPaneStyle.Height(m.Height - 4).Width(listWidth/2 - 2)
-		// singlePaneStyle = lipgloss.NewStyle().
-		//	Border(lipgloss.NormalBorder(), false, false, false, false).
-		//	MarginRight(2).
-		//	Height(m.Height - 5).
-		//	Width(listWidth - 2)
-		// dualPaneStyle = lipgloss.NewStyle().
-		//	Border(lipgloss.NormalBorder(), false, true, false, false).
-		//	MarginRight(2).
-		//	Height(m.Height - 5).
-		//	Width(listWidth/2 - 2)
+		m.max = m.Height - 1
+		singlePaneStyle = singlePaneStyle.Height(m.Height - 2).Width(listWidth - 2)
+		dualPaneStyle = dualPaneStyle.Height(m.Height - 2).Width(listWidth/2 - 2)
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.KeyMap.GoToTop):
