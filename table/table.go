c65808be7079fc07d7da07b47e3ae1d0ac2ad434
Sergio <sergio.macia@gitkraken.com>
2023-11-23 16:38:41 +0100

    remove custom min and max fn in favor of using golang's builtin methods

--- table/table.go
+++ table/table.go
@@ -414,22 +414,6 @@ func (m *Model) renderRow(rowID int) string {
 	return row
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
 func clamp(v, low, high int) int {
 	return min(max(v, low), high)
 }
