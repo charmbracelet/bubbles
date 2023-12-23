bb3190590e65d620748df3a15a6973aeb405ae94
Maas Lalani <maas@lalani.dev>
2023-12-23 13:56:55 -0500

    Revert "fix: truncate textinput placeholder (#442)"
    This reverts commit 9a0ff2ba4dc3e7d7a8905b691c6af102ceb48238.

--- textinput/textinput.go
+++ textinput/textinput.go
@@ -714,11 +714,7 @@ func (m Model) placeholderView() string {
 	v += m.Cursor.View()
 
 	// The rest of the placeholder text
-	var spaces []rune
-	for i := 0; i < m.Width-lipgloss.Width(string(p[1:m.Width])); i++ {
-		spaces = append(spaces, ' ')
-	}
-	v += style(string(append(p[1:m.Width], spaces...)))
+	v += style(string(p[1:]))
 
 	return m.PromptStyle.Render(m.Prompt) + v
 }
