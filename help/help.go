bdb76b4c52b05af85e165132ca855d44532a9637
ardnew <andrew@ardnew.com>
2024-03-27 17:03:14 -0500

    feat(key): add support for Help callback

--- help/help.go
+++ help/help.go
@@ -121,7 +121,7 @@ func (m Model) ShortHelpView(bindings []key.Binding) string {
 	var separator = m.Styles.ShortSeparator.Inline(true).Render(m.ShortSeparator)
 
 	for i, kb := range bindings {
-		if !kb.Enabled() {
+		if !kb.Enabled() || !kb.HelpAvailable() {
 			continue
 		}
 
@@ -188,7 +188,7 @@ func (m Model) FullHelpView(groups [][]key.Binding) string {
 
 		// Separate keys and descriptions into different slices
 		for _, kb := range group {
-			if !kb.Enabled() {
+			if !kb.Enabled() || !kb.HelpAvailable() {
 				continue
 			}
 			keys = append(keys, kb.Help().Key)
