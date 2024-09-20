d565b952245251f4afc13cddf5f2e910c7430faf
Broderick Westrope <broderickwestrope@gmail.com>
2024-09-20 10:27:20 +1000

    feat: add ignore max & min to IntState

--- picker/intstate.go
+++ picker/intstate.go
@@ -3,13 +3,17 @@ package picker
 type IntState struct {
 	min       int
 	max       int
+	ignoreMin bool
+	ignoreMax bool
 	selection int
 }
 
-func NewIntState(min, max int) *IntState {
+func NewIntState(min, max int, ignoreMin, ignoreMax bool) *IntState {
 	return &IntState{
 		min:       min,
 		max:       max,
+		ignoreMin: ignoreMin,
+		ignoreMax: ignoreMax,
 		selection: min,
 	}
 }
@@ -20,6 +24,9 @@ func (s *IntState) GetValue() interface{} {
 
 func (s *IntState) Next(canCycle bool) {
 	switch {
+	case s.ignoreMax:
+		s.selection++
+
 	case s.selection < s.max:
 		s.selection++
 
@@ -30,6 +37,9 @@ func (s *IntState) Next(canCycle bool) {
 
 func (s *IntState) Prev(canCycle bool) {
 	switch {
+	case s.ignoreMin:
+		s.selection--
+
 	case s.selection > s.min:
 		s.selection--
 
