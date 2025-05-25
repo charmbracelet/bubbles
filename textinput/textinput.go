7e5098cf3d342a4e0db44c77e51b8d67382c4ff6
Piya Gehi <pjg11@users.noreply.github.com>
2025-05-25 17:51:07 +0400

    chore(textinput): move key bindings to separate file

--- textinput/textinput.go
+++ textinput/textinput.go
@@ -42,49 +42,6 @@ const (
 // ValidateFunc is a function that returns an error if the input is invalid.
 type ValidateFunc func(string) error
 
-// KeyMap is the key bindings for different actions within the textinput.
-type KeyMap struct {
-	CharacterForward        key.Binding
-	CharacterBackward       key.Binding
-	WordForward             key.Binding
-	WordBackward            key.Binding
-	DeleteWordBackward      key.Binding
-	DeleteWordForward       key.Binding
-	DeleteAfterCursor       key.Binding
-	DeleteBeforeCursor      key.Binding
-	DeleteCharacterBackward key.Binding
-	DeleteCharacterForward  key.Binding
-	LineStart               key.Binding
-	LineEnd                 key.Binding
-	Paste                   key.Binding
-	AcceptSuggestion        key.Binding
-	NextSuggestion          key.Binding
-	PrevSuggestion          key.Binding
-}
-
-// DefaultKeyMap is the default set of key bindings for navigating and acting
-// upon the textinput.
-func DefaultKeyMap() KeyMap {
-	return KeyMap{
-		CharacterForward:        key.NewBinding(key.WithKeys("right", "ctrl+f")),
-		CharacterBackward:       key.NewBinding(key.WithKeys("left", "ctrl+b")),
-		WordForward:             key.NewBinding(key.WithKeys("alt+right", "ctrl+right", "alt+f")),
-		WordBackward:            key.NewBinding(key.WithKeys("alt+left", "ctrl+left", "alt+b")),
-		DeleteWordBackward:      key.NewBinding(key.WithKeys("alt+backspace", "ctrl+w")),
-		DeleteWordForward:       key.NewBinding(key.WithKeys("alt+delete", "alt+d")),
-		DeleteAfterCursor:       key.NewBinding(key.WithKeys("ctrl+k")),
-		DeleteBeforeCursor:      key.NewBinding(key.WithKeys("ctrl+u")),
-		DeleteCharacterBackward: key.NewBinding(key.WithKeys("backspace", "ctrl+h")),
-		DeleteCharacterForward:  key.NewBinding(key.WithKeys("delete", "ctrl+d")),
-		LineStart:               key.NewBinding(key.WithKeys("home", "ctrl+a")),
-		LineEnd:                 key.NewBinding(key.WithKeys("end", "ctrl+e")),
-		Paste:                   key.NewBinding(key.WithKeys("ctrl+v")),
-		AcceptSuggestion:        key.NewBinding(key.WithKeys("tab")),
-		NextSuggestion:          key.NewBinding(key.WithKeys("down", "ctrl+n")),
-		PrevSuggestion:          key.NewBinding(key.WithKeys("up", "ctrl+p")),
-	}
-}
-
 // Model is the Bubble Tea model for this text input element.
 type Model struct {
 	Err error
